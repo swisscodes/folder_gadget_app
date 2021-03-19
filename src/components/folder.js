a29f7ab649ad1bf69f85d2462088358788d351cc
Swissbobo <swissbobomail.com>
2021-03-19 15:48:50 +0300

    Added the samantic icons

--- src/components/folder.js
+++ src/components/folder.js
@@ -4,15 +4,18 @@ import {ShowChildren} from '../context/showchildren';
 export default function Folder(props) {
     const {state, setState} = useContext(ShowChildren);
     const HandleClick = () => setState(!state);
+    const direction = state ? "down" : "right";
     return (
         <div>
-            <span onClick={HandleClick}><i className="blue folder icon"></i></span>
+            <span onClick={HandleClick}>
+            <i className="blue folder icon"></i>
+            <i className={`caret ${direction} icon`}></i>
+            </span>
                 {props.name}
             <div style={{ marginLeft: "17px" }}>
                 {state ? props.children : null}
             </div>
         </div>
-
         
     );
 }
